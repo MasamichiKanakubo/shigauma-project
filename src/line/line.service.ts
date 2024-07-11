7b35f8f1c4c9a4c0f213fe7e8112b6b1982479f6
MasamichiKanakubo <renlijinjiubao808@gmail.com>
2024-07-11 17:59:28 +0900

    style: del else

--- src/line/line.service.ts
+++ src/line/line.service.ts
@@ -1,8 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import {
-  WebhookEvent,
-  WebhookRequestBody,
-} from '@line/bot-sdk';
+import { WebhookEvent, WebhookRequestBody } from '@line/bot-sdk';
 import { ConfigService as NestConfigService } from './config/config.service';
 
 @Injectable()
@@ -16,55 +13,56 @@ export class LineService {
     events.forEach((event) => {
       if (event.type === 'message') {
         if (
-          event.message.type === 'text' &&
-          event.message.text === '店舗を検索する'
+          event.message.type !== 'text' ||
+          event.message.text !== '店舗を検索する'
         ) {
-          client.replyMessage({
-            replyToken: event.replyToken,
-            messages: [
-              {
-                type: 'text',
-                text: '店舗のエリアを指定してください',
-                quickReply: {
-                  items: [
-                    {
-                      type: 'action',
-                      action: {
-                        type: 'message',
-                        label: '草津エリア',
-                        text: '草津エリア',
-                      },
-                    },
-                    {
-                      type: 'action',
-                      action: {
-                        type: 'message',
-                        label: '南草津エリア',
-                        text: '南草津エリア',
-                      },
-                    },
-                    {
-                      type: 'action',
-                      action: {
-                        type: 'message',
-                        label: '栗東エリア',
-                        text: '栗東エリア',
-                      },
-                    },
-                    {
-                      type: 'action',
-                      action: {
-                        type: 'message',
-                        label: '瀬田エリア',
-                        text: '瀬田エリア',
-                      },
-                    },
-                  ],
-                },
-              },
-            ],
-          });
+          return;
         }
+        client.replyMessage({
+          replyToken: event.replyToken,
+          messages: [
+            {
+              type: 'text',
+              text: '店舗のエリアを指定してください',
+              quickReply: {
+                items: [
+                  {
+                    type: 'action',
+                    action: {
+                      type: 'message',
+                      label: '草津エリア',
+                      text: '草津エリア',
+                    },
+                  },
+                  {
+                    type: 'action',
+                    action: {
+                      type: 'message',
+                      label: '南草津エリア',
+                      text: '南草津エリア',
+                    },
+                  },
+                  {
+                    type: 'action',
+                    action: {
+                      type: 'message',
+                      label: '栗東エリア',
+                      text: '栗東エリア',
+                    },
+                  },
+                  {
+                    type: 'action',
+                    action: {
+                      type: 'message',
+                      label: '瀬田エリア',
+                      text: '瀬田エリア',
+                    },
+                  },
+                ],
+              },
+            },
+          ],
+        });
       }
     });
   }
